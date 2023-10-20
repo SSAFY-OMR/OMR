9242b832ab293e2af2e6e6704d6c4a23cb2805ea
inputTitleHere <seanbryan97@naver.com>
2023-10-20 17:45:58 +0900

    fix: InterviewQuestion의 InterviewCategory를 ERD상 올바른 방향으로 수정

--- backend/omr/src/main/java/com/ssafy/omr/modules/interview/domain/InterviewQuestion.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/interview/domain/InterviewQuestion.java
@@ -4,26 +4,27 @@ import com.ssafy.omr.modules.util.base.BaseEntity;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @SuperBuilder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AttributeOverride(name = "id",column = @Column(name = "interview_question_id"))
+@AttributeOverride(name = "id", column = @Column(name = "interview_question_id"))
 @Entity
 public class InterviewQuestion extends BaseEntity {
 
-    @OneToMany(mappedBy = "interview_category_id")
-    private List<InterviewCategory> interviewCategories = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interview_category_id")
+    private InterviewCategory interviewCategory;
 
     @Column(columnDefinition = "TEXT")
     private String content;
