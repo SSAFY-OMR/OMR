ec89705dd2cf495588b84645e15aeca38252fcdf
신현철 <shin421179@gmail.com>
2023-10-23 17:07:03 +0900

    fix : 로그인 요청 validation 정규식 수정

--- backend/omr/src/main/java/com/ssafy/omr/modules/auth/dto/LoginRequest.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/auth/dto/LoginRequest.java
@@ -1,15 +1,19 @@
 package com.ssafy.omr.modules.auth.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 public record LoginRequest(
-        @Length(min = 2, max = 10, message = "올바르지 않은 아이디 형식입니다.")
-        @Pattern(regexp = "/^[a-z]+[a-z0-9]{5,19}$/", message = "올바르지 않은 아이디 형식입니다.")
+
+        @NotNull
+        @Length(min = 8, max = 16, message = "올바르지 않은 아이디 형식입니다.")
+        @Pattern(regexp = "^[a-z]+[a-z0-9]{8,16}$", message = "올바르지 않은 아이디 형식입니다.1")
         String loginId,
 
+        @NotNull
         @Length(min = 8, max = 16, message = "올바르지 않은 비밀번호 형식입니다.")
-        @Pattern(regexp = "/^(?=.\\d)(?=.[a-zA-Z])[0-9a-zA-Z]{8,16}$/", message = "올바르지 않은 비밀번호 형식입니다.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,16}$", message = "올바르지 않은 비밀번호 형식입니다.1")
         String password
 ) {
 }
