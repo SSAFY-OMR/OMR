b6f05c5f802da76faf364895fefd610b7858ab54
Yong <amico741@naver.com>
2023-10-23 21:47:37 +0900

    feat: 나의 프로필 조회 비즈니스 로직 추가

--- backend/omr/src/main/java/com/ssafy/omr/modules/member/service/MemberService.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/member/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.ssafy.omr.modules.member.service;
 
+import com.ssafy.omr.modules.member.domain.Member;
+import com.ssafy.omr.modules.member.dto.MemberProfileResponse;
+import com.ssafy.omr.modules.member.exception.MemberNotFoundException;
+import com.ssafy.omr.modules.member.mapper.MemberMapper;
 import com.ssafy.omr.modules.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    public MemberProfileResponse getMyProfileInformation(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+
+        return MemberMapper.supplyMemberProfileResponseFrom(member);
+    }
 }
