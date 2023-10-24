1dd858611dace6787c912c0a6c2cf5a464c68490
imhynni <omelas329@gmail.com>
2023-10-24 14:25:17 +0900

    feat: 버튼 컴포넌트 구현

--- frontend/omr/src/styles/color.ts
+++ frontend/omr/src/styles/color.ts
@@ -1,4 +1,7 @@
+export const WHITE = '#FFFFFF';
+
 export const NORMAL_BLUE = '#18b3e7';
+export const BLUE_50 = '#e8f7fd';
 export const BLUE_100 = '#b7e7f8';
 export const BLUE_200 = '#95dcf4';
 export const BLUE_300 = '#64ccef';
@@ -9,4 +12,5 @@ export const BLUE_700 = '#117fa4';
 export const BLUE_800 = '#0d627f';
 export const BLUE_900 = '#0a4b61';
 
+export const NEUTRAL_20 = '#f5f6f7';
 export const NEUTRAL_40 = '#dfe2e6';
