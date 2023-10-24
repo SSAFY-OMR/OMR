466460c059afd07255257ad737b178505ccd1fd0
khs <anjfkrhdkdle@naver.com>
2023-10-24 09:31:59 +0900

    feat: 메타데이터 컨트롤러 추가 - 카테고리 조회 api 추가

---
+++ backend/omr/src/main/java/com/ssafy/omr/modules/meta/controller/MetaDataController.java
@@ -0,0 +1,33 @@
+package com.ssafy.omr.modules.meta.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.omr.modules.meta.domain.InterviewCategory;
+import com.ssafy.omr.modules.meta.dto.MetaDataElement;
+import com.ssafy.omr.modules.meta.dto.MetaDataResponse;
+import com.ssafy.omr.modules.meta.mapper.MetaDataMapper;
+import com.ssafy.omr.modules.util.base.BaseResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class MetaDataController {
+
+	@RequestMapping("/category")
+	public BaseResponse<MetaDataResponse> getInterviewCategories() {
+		List<MetaDataElement> categories = Arrays.stream(InterviewCategory.values())
+			.map(MetaDataMapper::supplyMetaDataElementFromCategory)
+			.toList();
+
+		return BaseResponse.<MetaDataResponse>builder()
+			.data(new MetaDataResponse(categories))
+			.build();
+	}
+}
