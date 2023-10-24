bc00af3495b0dd986f50db54c049f65e72de264e
khs <anjfkrhdkdle@naver.com>
2023-10-24 10:44:09 +0900

    fix: MetaData Controller Mapper 수정사항 반영

--- backend/omr/src/main/java/com/ssafy/omr/modules/meta/controller/MetaDataController.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/meta/controller/MetaDataController.java
@@ -1,13 +1,9 @@
 package com.ssafy.omr.modules.meta.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.omr.modules.meta.domain.InterviewCategory;
-import com.ssafy.omr.modules.meta.dto.MetaDataElement;
 import com.ssafy.omr.modules.meta.dto.MetaDataResponse;
 import com.ssafy.omr.modules.meta.mapper.MetaDataMapper;
 import com.ssafy.omr.modules.util.base.BaseResponse;
@@ -22,12 +18,8 @@ public class MetaDataController {
 
 	@GetMapping("/category")
 	public BaseResponse<MetaDataResponse> getInterviewCategories() {
-		List<MetaDataElement> categories = Arrays.stream(InterviewCategory.values())
-			.map(MetaDataMapper::supplyMetaDataElementFromCategory)
-			.toList();
-
 		return BaseResponse.<MetaDataResponse>builder()
-			.data(new MetaDataResponse(categories))
+			.data(MetaDataMapper.suppleMetaDataResponseFrom(InterviewCategory.values()))
 			.build();
 	}
 }
