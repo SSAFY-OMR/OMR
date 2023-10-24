b5597e15da4ad9ba24966e5d076885a4a52a7d29
신현철 <shin421179@gmail.com>
2023-10-24 13:40:23 +0900

    feat : 외부 호출 예외처리 추가

--- backend/omr/src/main/java/com/ssafy/omr/modules/util/RandomUtil.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/util/RandomUtil.java
@@ -1,5 +1,6 @@
 package com.ssafy.omr.modules.util;
 
+import com.ssafy.omr.modules.global.exception.ExternalLibraryException;
 import com.ssafy.omr.modules.member.dto.NicknameResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,13 @@ public class RandomUtil {
     private final RestTemplate restTemplate;
     private static final String RANDOM_NICKNAME_GENERATOR_URL = "https://nickname.hwanmoo.kr/?format=json&max_length=12";
     public String generateNickname() {
-        NicknameResponse nicknameResponse = restTemplate.getForObject(RANDOM_NICKNAME_GENERATOR_URL, NicknameResponse.class);
+        NicknameResponse nicknameResponse;
+
+        try {
+            nicknameResponse = restTemplate.getForObject(RANDOM_NICKNAME_GENERATOR_URL, NicknameResponse.class);
+        } catch (Exception e) {
+            throw new ExternalLibraryException("hwanmoo.kr");
+        }
 
         return nicknameResponse.words().get(0);
     }
