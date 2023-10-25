6825b88f1beb8e75cdeb826c3d73ba1a468000d9
imhynni <omelas329@gmail.com>
2023-10-25 12:58:40 +0900

    feat: read, edit type 속성 추가

--- frontend/omr/src/components/AnswerInput/index.tsx
+++ frontend/omr/src/components/AnswerInput/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 
 import styles from './index.module.scss';
+import Button from '../common/Button';
 
-const AnswerInput = () => {
+type AnswerInputProps = {
+  type: 'edit' | 'read';
+  content?: string;
+};
+
+const AnswerInput = ({ type, content }: AnswerInputProps) => {
   return (
     <div className={styles.AnswerInput}>
-      <div className={styles.answerAreaWrapper}>
-        <div className={styles.answerArea} contentEditable />
+      {type === 'edit' ? (
+        <div className={styles.answerAreaWrapper}>
+          <div className={styles.answerArea} contentEditable />
+        </div>
+      ) : (
+        <div className={styles.answerAreaWrapper}>
+          <div className={styles.answerArea}>{content}</div>
+        </div>
+      )}
+      <div className={styles.saveBtn}>
+        {type === 'edit' ? (
+          <Button
+            color="secondary"
+            size="small"
+            type="complete"
+            width="fitContent"
+          >
+            저장
+          </Button>
+        ) : (
+          <Button color="secondary" size="small" type="edit" width="fitContent">
+            수정
+          </Button>
+        )}
       </div>
     </div>
   );
