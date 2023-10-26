7512acba71966c4773736cc08236d4058a24d7bd
jjuny7712 <jjuny7712@gmail.com>
2023-10-26 17:37:28 +0900

    docs: 스웨거 명세 작성

--- backend/omr/src/main/java/com/ssafy/omr/modules/question/dto/BasePageRequest.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/question/dto/BasePageRequest.java
@@ -1,20 +1,24 @@
 package com.ssafy.omr.modules.question.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+@Schema(description = "페이지 관련 DTO")
 @Getter
 @Setter
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BasePageRequest {
+    @Schema(description = "현재 페이지", defaultValue = "1", minimum = "1")
     @Builder.Default
     private Integer page = 1;
 
+    @Schema(description = "페이지 크기", defaultValue = "10", minimum = "10")
     @Min(value = 10, message = "size가 너무 작습니다.")
     @Builder.Default
     private Integer size = 10;
