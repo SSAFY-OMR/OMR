4801b50c7584e4439f97eb54819fa43a01fc7bfe
imhynni <omelas329@gmail.com>
2023-10-26 17:36:04 +0900

    feat: 토스트 컴포넌트 구현

--- frontend/omr/.storybook/preview.tsx
+++ frontend/omr/.storybook/preview.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
+
 import type { Preview } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import '../src/styles/global.scss';
 
 const preview: Preview = {
   parameters: {
+    viewport: { viewports: INITIAL_VIEWPORTS, defaultViewport: 'iPhone 12' },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
@@ -21,6 +25,14 @@ const preview: Preview = {
       ],
     },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ overflow: 'auto', minHeight: '300px' }}>
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
