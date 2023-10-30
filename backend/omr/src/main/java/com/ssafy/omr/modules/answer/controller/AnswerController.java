862b6f600fe78bf43af99f6c3746861bc5972f99
inputTitleHere <seanbryan97@naver.com>
2023-10-30 12:32:29 +0900

    feat: sort default 및 page size 추가

--- backend/omr/src/main/java/com/ssafy/omr/modules/answer/controller/AnswerController.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/answer/controller/AnswerController.java
@@ -16,6 +16,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -30,6 +33,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AnswerController {
     private final AnswerService answerService;
+    private final int PAGE_SIZE = 5;
+
 
     @PostMapping("/create")
     public BaseResponse<CreateAnswerResponse> createAnswer(@LoginUser AuthInfo authInfo, @RequestBody CreateAnswerRequest createAnswerRequest) {
@@ -62,6 +67,13 @@ public class AnswerController {
     @GetMapping("/question/{questionId}")
     public BaseResponse<QuestionAnswerResponse> getAnswerList(
             @RequestParam("questionId") Long questionId,
+            @PageableDefault(
+                    size = PAGE_SIZE
+            )
+            @SortDefault.SortDefaults({
+                    @SortDefault(sort = "likeCount", direction = Sort.Direction.DESC),
+                    @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC)
+            })
             Pageable pageable
     ) {
         return BaseResponse.<QuestionAnswerResponse>builder()
