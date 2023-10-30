181cbf1a9357779df4c81f532a6b1fe57a60f04d
inputTitleHere <seanbryan97@naver.com>
2023-10-30 12:21:37 +0900

    feat: question에 대한 answer list 조회 코드 작성

--- backend/omr/src/main/java/com/ssafy/omr/modules/answer/controller/AnswerController.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/answer/controller/AnswerController.java
@@ -3,6 +3,7 @@ package com.ssafy.omr.modules.answer.controller;
 import com.ssafy.omr.modules.answer.dto.CreateAnswerRequest;
 import com.ssafy.omr.modules.answer.dto.CreateAnswerResponse;
 import com.ssafy.omr.modules.answer.dto.DeleteAnswerRequest;
+import com.ssafy.omr.modules.answer.dto.QuestionAnswerResponse;
 import com.ssafy.omr.modules.answer.dto.ToggleLikeAnswerRequest;
 import com.ssafy.omr.modules.answer.dto.ToggleLikeAnswerResponse;
 import com.ssafy.omr.modules.answer.dto.UpdateAnswerRequest;
@@ -10,12 +11,18 @@ import com.ssafy.omr.modules.answer.service.AnswerService;
 import com.ssafy.omr.modules.auth.dto.AuthInfo;
 import com.ssafy.omr.modules.auth.token.LoginUser;
 import com.ssafy.omr.modules.util.base.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -51,5 +58,16 @@ public class AnswerController {
                 .data(answerService.toggleAnswerLike(authInfo, toggleLikeAnswerRequest))
                 .build();
     }
+
+    @GetMapping("/question/{questionId}")
+    public BaseResponse<QuestionAnswerResponse> getAnswerList(
+            @RequestParam("questionId") Long questionId,
+            Pageable pageable
+    ) {
+        return BaseResponse.<QuestionAnswerResponse>builder()
+                .data(answerService.getAnswerList(questionId, pageable))
+                .build();
+    }
+
 
 }
