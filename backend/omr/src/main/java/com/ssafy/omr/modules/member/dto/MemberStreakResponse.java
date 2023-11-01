6c7fae895b9677faac1840e5c10b2d730fd22c77
YongsHub <amico741@naver.com>
2023-11-01 10:46:05 +0900

    docs: Member Domain Swagger 수정

--- backend/omr/src/main/java/com/ssafy/omr/modules/member/dto/MemberStreakResponse.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/member/dto/MemberStreakResponse.java
@@ -1,9 +1,13 @@
 package com.ssafy.omr.modules.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 import java.util.Map;
 
 public record MemberStreakResponse(
+
+        @Schema(description = "년도 월일에 따른 스트릭 개수", defaultValue = "{\"2023-10-30\": 2, \"2023-11-01\": 3}")
         Map<LocalDate, Integer> streaks,
         int currentStreak,
         int longestStreak
