ad6b3e16bc796669f619b3c1b5ce94f9980251ff
imhynni <omelas329@gmail.com>
2023-11-03 13:27:14 +0900

    fix: recoil localstorage 에러 수정

--- frontend/omr/src/states/auth.ts
+++ frontend/omr/src/states/auth.ts
@@ -1,6 +1,9 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
+const localStorage =
+  typeof window !== `undefined` ? window.localStorage : undefined;
+
 const { persistAtom } = recoilPersist({
   key: 'USER', // 고유한 key 값
   storage: localStorage,
@@ -9,5 +12,11 @@ const { persistAtom } = recoilPersist({
 export const userTokenState = atom({
   key: 'userTokenState',
   default: '',
+  effects: [persistAtom],
+});
+
+export const userInfoState = atom({
+  key: 'userInfoState',
+  default: {},
   effects: [persistAtom],
 });
