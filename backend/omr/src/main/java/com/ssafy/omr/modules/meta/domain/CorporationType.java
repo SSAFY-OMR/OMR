fa71443f7742460ba98fd1778bcce9a30768308c
jjuny7712 <jjuny7712@gmail.com>
2023-11-06 15:33:16 +0900

    feat: 기업별 면접 질문 관련 엔티티 생성

---
+++ backend/omr/src/main/java/com/ssafy/omr/modules/meta/domain/CorporationType.java
@@ -0,0 +1,34 @@
+package com.ssafy.omr.modules.meta.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ssafy.omr.modules.meta.exception.InterviewCategoryBadRequestException;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum CorporationType implements MetaData {
+    NAVER(1, "NAVER", "네이버"),
+    KAKAO(2, "KAKAO", "카카오"),
+    WOOWA_BROTHERS(3, "WOOWA_BROTHERS", "우아한형제들"),
+    LINE_PLUS(4, "LINE_PLUS", "라인플러스"),
+    SAMSUNG(5, "SAMSUNG", "삼성");
+
+    private final Integer id;
+    private final String name;
+    private final String description;
+
+    CorporationType(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public static CorporationType ofName(String name) {
+        return Arrays.stream(CorporationType.values())
+                .filter(value -> value.getName().equals(name))
+                .findAny()
+                .orElseThrow(InterviewCategoryBadRequestException::new);
+    }
+}
