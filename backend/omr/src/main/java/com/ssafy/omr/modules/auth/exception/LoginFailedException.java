a61f1af234a50b9789192dc6585d8c5d31b7cc9d
YongsHub <amico741@naver.com>
2023-11-09 13:20:12 +0900

    fix: 로그인 실패 시, 예외 처리 수정

--- backend/omr/src/main/java/com/ssafy/omr/modules/auth/exception/LoginFailedException.java
+++ backend/omr/src/main/java/com/ssafy/omr/modules/auth/exception/LoginFailedException.java
@@ -1,9 +1,9 @@
 package com.ssafy.omr.modules.auth.exception;
 
-import com.ssafy.omr.modules.global.exception.UnauthorizedException;
+import com.ssafy.omr.modules.global.exception.NotFoundException;
 
-public class LoginFailedException extends UnauthorizedException {
-    private static final String MESSAGE = "아이디나 비밀번호가 잘못되었습니다";
+public class LoginFailedException extends NotFoundException {
+    private static final String MESSAGE = "아이디나 비밀번호가 잘못되었거나 존재하지 않습니다";
 
     public LoginFailedException() {
         super(MESSAGE);
